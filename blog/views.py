229da60aada0adad1fc09b0099148ad240c2733d
Johann Blignaut <blignaut24@gmail.com>
2024-06-23 10:08:59 +0000

    fix: correct postlist view with pagination

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# ---------------------
+# Django Imports
+# ---------------------
+# Import generic views from Django
+from django.views import generic
 
-# Create your views here.
+# Import Blogpost and Comment models
+from .models import Blogpost, Comment
+
+# ---------------------
+# PostList View
+# ---------------------
+# This class-based view displays all blogposts that are marked as published (status=1),
+# ordered by the date they were created. It uses pagination to display a set number of posts per page.
+class PostList(generic.ListView):
+    model = Blogpost
+    queryset = Blogpost.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 8
