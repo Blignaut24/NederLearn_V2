79d83fc6008c75b9e8ee811f2044ae1cc427d610
Johann Blignaut <blignaut24@gmail.com>
2024-06-24 20:08:45 +0000

    feat: implement index.html view for improved navigation and content management

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+# jshint esversion: 6
+
 # ---------------------
 # Django Imports
 # ---------------------
@@ -7,23 +9,47 @@ from django.shortcuts import render
 # Import generic views from Django
 from django.views import generic
 
-# Import Blogpost and Comment models
-from .models import Blogpost, Comment
+# Import Blogpost model
+from .models import Blogpost
 
 # ---------------------
-# Define home function
+# BlogPostList View
 # ---------------------
-# This function renders the base.html page when the home URL is requested.
-def home(request):
-    return render(request, 'base.html')
-
-# ---------------------
-# PostList View
-# ---------------------
-# This class-based view displays all blogposts that are marked as published (status=1),
-# ordered by the date they were created. It uses pagination to display a set number of posts per page.
-class PostList(generic.ListView):
+# This class-based view displays a list of published blogposts, sorted by creation date. The list is paginated.
+class BlogPostList(generic.ListView):
     model = Blogpost
-    queryset = Blogpost.objects.filter(status=1).order_by('created_on')
+    context_object_name = 'blogposts'
     template_name = 'index.html'
-    paginate_by = 8
+    paginate_by = 6
+
+    # ---------------------
+    # Dispatch Method
+    # ---------------------
+    # This method is called before any other method in the view. It checks if
+    # the user is authenticated. If not, they are redirected to the login page.
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('account_login')
+        return super().dispatch(request, *args, **kwargs)
+
+    # ---------------------
+    # Get Queryset Method
+    # ---------------------
+    # This method filters blogposts by media category and orders them by creation date.
+    def get_queryset(self):
+        queryset = Blogpost.objects.filter(status=1).order_by('-created_on')
+        media_category = self.request.GET.get('category')
+        if media_category:
+            queryset = queryset.filter(
+                media_category__media_name=media_category
+                )
+        return queryset
+
+    # ---------------------
+    # Get Context Data Method
+    # ---------------------
+    # This method adds media categories to the context for filtering in the template.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = MediaCategory.objects.all()
+        return context
