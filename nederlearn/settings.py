e7b94ee1db126ede787a475c0414c0fe19d93416
Johann Blignaut <blignaut24@gmail.com>
2024-06-26 17:30:41 +0000

    feat: add logout.html for user signout

--- nederlearn/settings.py
+++ nederlearn/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGOUT_REDIRECT_URL = '/accounts/logout.html'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
