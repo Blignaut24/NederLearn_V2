6ec7887c92860af644148e46306780fecfdc6d6d
Johann Blignaut <blignaut24@gmail.com>
2024-06-26 18:34:49 +0000

    fix: update logout.html

--- nederlearn/settings.py
+++ nederlearn/settings.py
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/account/logout.html'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
