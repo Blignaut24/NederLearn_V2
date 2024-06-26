ef5722953318c2ae642eff0276b5b77185ed8be8
Johann Blignaut <blignaut24@gmail.com>
2024-06-26 21:06:56 +0000

    fix: set comment approval status to false

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
-from .models import Blogpost, MediaCategory
+from .models import Blogpost
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
@@ -81,7 +81,7 @@ class BlogPostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Blogpost.objects.filter(status=1)
         blogpost = get_object_or_404(queryset, slug=slug)
-        comments = blogpost.comments.filter(approved=True).order_by("created_on")
+        comments = blogpost.comments.filter(approved=False).order_by("created_on")
         liked = False
         if blogpost.likes.filter(id=self.request.user.id).exists():
             liked = True
